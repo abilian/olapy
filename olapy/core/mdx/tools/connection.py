795ad74c0131f036d818f53bfc5bd0b052d73416
mouadh <mouadh.kaabachi@esprit.tn>
2017-05-09 17:00:38 +0200

    change connection settings

--- olapy/core/mdx/tools/connection.py
+++ olapy/core/mdx/tools/connection.py
@@ -1,13 +1,17 @@
 import psycopg2 as pg
 
+# postgres connection
+USERNAME = 'postgres'
+PASSWORD = 'root'
+HOST = 'localhost'
 
 class MyDB(object):
     """Connect to sql database (postgres only right now)."""
     def __init__(self,
-                 username='postgres',
-                 password='root',
+                 username=USERNAME,
+                 password=PASSWORD,
                  db=None,
-                 host='localhost'):
+                 host=HOST):
         if db is None:
             self.connection = pg.connect(
                 "user={0} password={1}".format(username, password))
