30051e99967d15f57a232984fb20c6f447f284bd
mouadh <mouadh.kaabachi@esprit.tn>
2017-05-15 13:29:53 +0200

    fix update

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, division, print_function
+from __future__ import division, print_function
 
 import pandas as pd
 from pandas.util.testing import assert_frame_equal
