0f8cb74f9a526a666e7b36f0a968ecc9d5a4c2c6
mouadh <mouadh.kaabachi@esprit.tn>
2017-06-08 13:14:17 +0200

    update results

--- olapy/core/mdx/tools/mem_bench.py
+++ olapy/core/mdx/tools/mem_bench.py
@@ -19,5 +19,7 @@ def memory_usage(where):
     with open(os.path.join(expanduser('~'), 'bech_mem.txt'), mode='a+') as file:
         mem_summary = summary.summarize(muppy.get_objects())
         file.write("Memory summary:" + where + '\n\n')
+        print("Memory summary:" + where )
         summary.print_(mem_summary, limit=2)
+        print('----------------------------')
         file.write("VM: %.2fMb" % (get_virtual_memory_usage_kb() / 1024.0) + '\n\n')
