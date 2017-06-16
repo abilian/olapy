4580878022225bddadf60aead30bc3372d84c60c
mouadh <mouadh.kaabachi@esprit.tn>
2017-06-16 17:11:43 +0200

    update path

--- olapy/core/mdx/executor/execute.py
+++ olapy/core/mdx/executor/execute.py
@@ -244,7 +244,7 @@ class MdxEngine:
         :return: path to the cube
         """
         if MdxEngine.DATA_FOLDER is not None:
-            return os.path.join(MdxEngine.DATA_FOLDER,'olapy-data', MdxEngine.CUBE_FOLDER, self.cube)
+            return os.path.join(MdxEngine.DATA_FOLDER, MdxEngine.CUBE_FOLDER, self.cube)
         return os.path.join(self.cube_path, self.cube)
 
     # TODO temporary function
