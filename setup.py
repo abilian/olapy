9aff0521d9bac30c94fa5dfb1f5a6e764bb6009b
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2017-10-09 12:31:11 +0200

    temporary remove mysql-python (python 3 installation problem)

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6"
         # "Topic :: Business intelligence",
     ],)
 
