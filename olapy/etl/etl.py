89e118d9c488bcf3d345729fbea9a0cd6048021b
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2017-10-11 12:34:25 +0200

    v1

--- olapy/etl/etl.py
+++ olapy/etl/etl.py
@@ -1,14 +1,24 @@
 import bonobo
-from bonobo.commands.run import get_default_services
+import os
 
 from olapy.core.mdx.executor.execute import MdxEngine
 
 
 class ETL(object):
-    def __init__(self, source_type):
+    def __init__(self, source_type, headers, target_cube='etl_cube'):
+        """
+
+        :param source_type: csv | file | pickle
+        :param headers:
+        """
         self.source_type = source_type
         self.cube_path = MdxEngine._get_default_cube_directory()
         self.seperator = self._get_default_seperator()
+        self.headers = headers if headers else []
+        self.target_cube = target_cube
+        self.olapy_cube_path = os.path.join(MdxEngine._get_default_cube_directory())
+        if not os.path.isdir(os.path.join(self.olapy_cube_path, MdxEngine.CUBE_FOLDER, self.target_cube)):
+            os.makedirs(os.path.join(self.olapy_cube_path, MdxEngine.CUBE_FOLDER, self.target_cube))
 
     def _get_default_seperator(self):
         if self.source_type.upper() in ['CSV', 'FILE']:
@@ -20,9 +30,14 @@ class ETL(object):
         :param table_type: facts | dimension
         :return:
         """
-        return line.split(self.seperator)
+        # return line
+        transformed = {}
+        splited = line.split(self.seperator, maxsplit=len(self.headers))
+        for idx, head in enumerate(self.headers):
+            transformed.update({head: splited[idx]})
+        return transformed
 
-    def extract(self, file):
+    def extract(self, file, facts=False):
         """
 
         :param file: file | csv | json | pickle
@@ -30,35 +45,20 @@ class ETL(object):
         """
         return getattr(bonobo, self.source_type.title() + "Reader")(file)
 
-    def load(self):
-        # if not os.path.isdir(os.path.join(self.cube_path, CUBE_NAME)):
-        #     os.makedirs(os.path.join(self.cube_path, CUBE_NAME))
-        # cube_path = os.path.join(self.cube_path, CUBE_NAME)
-        # for (table_name, table_value) in tables.items():
-        #     table_value.to_csv(
-        #         os.path.join(os.path.join(cube_path, table_name + '.csv')),
-        #         sep=";",
-        #         index=False)
+    def load(self, target='csv'):
+        self.target_cube = os.path.join(self.olapy_cube_path, self.target_cube)
 
-        return bonobo.CsvWriter('coffeeshops.csv')
-        # return  bonobo.JsonWriter('coffeeshops.json', fs='fs.output', ioformat='arg0')
+        # todo target postgres, mysql ....
+        if target.upper() == 'CSV':
+            return bonobo.CsvWriter('coffeeshops.csv', ioformat='arg0')
 
 
-etl = ETL('file')
+etl = ETL(source_type='file', headers=['col1', 'col2', 'col3', 'col4'])
 
 if __name__ == '__main__':
     graph = bonobo.Graph(
-        # etl.extract('coffeeshops.txt'),
-        bonobo.CsvReader('coffeeshops.txt'),
+        etl.extract('test.txt'),
         etl.transform,
-        # bonobo.CsvWriter('coffeeshops.csv')
         etl.load())
 
-    def get_services():
-        return {
-            'fs': bonobo.open_examples_fs('datasets'),
-            'fs.output': bonobo.open_fs(),
-        }
-
-    bonobo.run(graph, services=get_default_services(__file__))
-    # bonobo.run(graph, services=get_services())
+    bonobo.run(graph)
