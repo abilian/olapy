2d95c9689b392ee43ff419231667dd5f6705f778
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2017-10-18 17:55:04 +0200

    file and csv

--- olapy/etl/etl.py
+++ olapy/etl/etl.py
@@ -35,7 +35,8 @@ class ETL(object):
         self.source_type = source_type
         self.facts_table = facts_table
         self.cube_path = MdxEngine._get_default_cube_directory()
-        self.seperator = self._get_default_seperator() if not separator else separator
+        self.seperator = self._get_default_seperator(
+        ) if not separator else separator
         self.target_cube = target_cube
         self.olapy_cube_path = os.path.join(
             MdxEngine._get_default_cube_directory(), MdxEngine.CUBE_FOLDER)
@@ -50,7 +51,15 @@ class ETL(object):
         if self.source_type.upper() in ['CSV', 'FILE']:
             return ','
 
-    def transform(self, line):
+    def add_id_prefix(self, header):
+        headers = []
+        for column_header in header:
+            if column_header in self.current_dim_id_column and '_id' not in column_header[-3:]:
+                column_header = column_header + '_id'
+            headers.append(column_header)
+        return headers
+
+    def transform_file(self, line):
         """
 
         :param table_type: facts | dimension
@@ -60,34 +69,45 @@ class ETL(object):
         transformed = {}
 
         if self.dim_first_row_headers:
-            # split headers
-            splited = line.split(self.seperator)
-            self.dim_headers = splited
+            self.dim_headers = self.add_id_prefix(line)
             self.dim_first_row_headers = False
-            for idx, column_header in enumerate(splited):
-                if column_header in self.current_dim_id_column and '_id' not in column_header[-3:]:
-                    splited[idx] = column_header + '_id'
 
-        else:
-            if self.dim_headers:
-                splited = line.split(
-                    self.seperator, maxsplit=len(self.dim_headers))
-            else:
-                # columns = self.current_dim_id_column
-                splited = line.split(self.seperator)
-
-            for idx, head in enumerate(self.dim_headers):
-                transformed.update({head: splited[idx]})
+        for idx, head in enumerate(self.dim_headers):
+            transformed.update({head: line[idx]})
 
         return transformed
 
-    def extract(self, file):
+    def transform_csv(self, kwargs):
+        if self.dim_first_row_headers:
+            for key in self.current_dim_id_column:
+                if '_id' not in key:
+                    kwargs[key + '_id'] = kwargs[key]
+                    del kwargs[key]
+        return kwargs
+
+    def transform(self, *args, **kwargs):
+        """
+
+        :param table_type: facts | dimension
+        :return:
+        """
+        if self.source_type.upper() == 'FILE':
+            return self.transform_file(args)
+
+        elif self.source_type.upper() == 'CSV':
+            return self.transform_csv(kwargs)
+
+    def extract(self, file, delimiter=';'):
         """
 
         :param file: file | csv | json | pickle
         :return:
         """
-        return getattr(bonobo, self.source_type.title() + "Reader")(file)
+        # todo clean this
+        if self.source_type.upper() == 'FILE':
+            return getattr(bonobo, self.source_type.title() + "Reader")(file)
+        elif self.source_type.upper() == 'CSV':
+            return getattr(bonobo, self.source_type.title() + "Reader")(file, **{'delimiter': delimiter})
 
     def load(self, table_name, target='csv'):
 
@@ -101,13 +121,16 @@ class ETL(object):
                 os.path.join(GEN_FOLDER, table_name + '.csv'), ioformat='arg0')
 
     def copy_2_olapy_dir(self):
-        if not os.path.isdir(os.path.join(self.olapy_cube_path, self.target_cube)):
+        if not os.path.isdir(
+                os.path.join(self.olapy_cube_path, self.target_cube)):
             os.makedirs(os.path.join(self.olapy_cube_path, self.target_cube))
 
         self.target_cube = os.path.join(self.olapy_cube_path, self.target_cube)
 
         for file in os.listdir(GEN_FOLDER):
-            copyfile(os.path.join(GEN_FOLDER, file), os.path.join(self.target_cube, file))
+            copyfile(
+                os.path.join(GEN_FOLDER, file),
+                os.path.join(self.target_cube, file))
 
     def get_source_extension(self):
         if self.source_type.upper() == 'FILE':
@@ -141,10 +164,8 @@ if __name__ == '__main__':
             etl.current_dim_id_column = dims_infos[table]
 
         graph = bonobo.Graph(
-            etl.extract(table + etl.get_source_extension()),
-            # transform,
-            etl.transform,
-            etl.load(table))
+            etl.extract(table + etl.get_source_extension(), delimiter=','),
+            etl.transform, etl.load(table))
 
         bonobo.run(graph)
 
