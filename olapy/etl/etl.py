5226fa24b86edd3b58de127875960d9a7648e024
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2017-10-19 12:26:29 +0200

    clean

--- olapy/etl/etl.py
+++ olapy/etl/etl.py
@@ -151,37 +151,3 @@ class ETL(object):
             return '.txt'
         elif self.source_type.upper() == 'CSV':
             return '.csv'
-
-
-if __name__ == '__main__':
-
-    # with extension
-    dims_infos = {
-        # 'dimension': ['col_id'],
-        'Geography': ['geokey']
-    }
-
-    facts_ids = ['geokey', 'Day', 'City', 'Licence']
-
-    etl = ETL(
-        source_type='csv',
-        facts_table='sales_facts',
-        facts_ids=facts_ids,
-        **dims_infos)
-
-    for table in list(dims_infos.keys()) + [etl.facts_table]:
-        # transform = Transform(dims_infos[table])
-        etl.dim_first_row_headers = True
-        if table == etl.facts_table:
-            etl.current_dim_id_column = facts_ids
-        else:
-            etl.current_dim_id_column = dims_infos[table]
-
-        graph = bonobo.Graph(
-            etl.extract(table + etl.get_source_extension(), delimiter=','),
-            etl.transform, etl.load(table))
-
-        bonobo.run(graph)
-
-    # temp ( bonobo can't export (save) to path (bonobo bug)
-    etl.copy_2_olapy_dir()
