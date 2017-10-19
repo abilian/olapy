6859c3c05beef80415e8b5d2b507e44a7ed8b68b
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2017-10-19 12:38:06 +0200

    clean

--- olapy/etl/etl.py
+++ olapy/etl/etl.py
@@ -46,7 +46,7 @@ class ETL(object):
         self.dim_headers = []
         if not os.path.exists(GEN_FOLDER):
             os.mkdir(GEN_FOLDER)
-        if not os.listdir(GEN_FOLDER) == []:
+        if not os.listdir(GEN_FOLDER):
             for file in GEN_FOLDER:
                 os.remove(file)
 
