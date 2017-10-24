4563bc86214acf125883e49ad0f3f78577e1cf75
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2017-10-24 16:30:05 +0200

    change _service work mysql postgres

--- olapy/etl/_services.py
+++ olapy/etl/_services.py
@@ -1,20 +1,47 @@
-import dotenv
-import logging
-from bonobo_sqlalchemy.util import create_postgresql_engine
+from collections import defaultdict
+from sqlalchemy import create_engine
 
-dotenv.load_dotenv(dotenv.find_dotenv())
-logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+# *****mysql default config****
+# DRIVER = 'mysql'
+# HOST = 'localhost'
+# PORT = '3306'
+# DB_NAME = 'sales_mysql'
+# USER = 'root'
+# PASSWORD = 'toor'
 
+# *****postgres default config****
+DRIVER = 'postgres'
+HOST = 'localhost'
+PORT = '5432'
 DB_NAME = 'tutorial'
-USER = 'postgres'
-PASS = 'root'
+USER = 'tutorial'
+PASSWORD = 'tutorial'
+
+DB_CONFIG_DEFAULTS = {
+    'driver': DRIVER,
+    'host': HOST,
+    'port': PORT,
+    'name': DB_NAME,
+    'user': USER,
+    'pass': PASSWORD,
+}
+
+DSN_TEMPLATE = '{driver}://{user}:{pass}@{host}:{port}/{name}'
+
+
+def create_db_engine(options='client_encoding=utf8'):
+    config = defaultdict(**DB_CONFIG_DEFAULTS)
+    dsn = DSN_TEMPLATE.format(**config)
+
+    if options and DB_CONFIG_DEFAULTS['driver'].upper() == 'POSTGRES':
+        dsn += '?' + options
+
+    print('Creating database engine: ' + dsn)
+
+    return create_engine(dsn)
 
 
 def get_services():
     return {
-        'sqlalchemy.engine':
-            create_postgresql_engine(
-                **{'name': DB_NAME,
-                   'user': USER,
-                   'pass': PASS})
+        'sqlalchemy.engine': create_db_engine()
     }
