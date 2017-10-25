4fd441664cf816e9d395505936776aa6b0504c12
Mouadh Kaabachi <mouadh.kaabachi@esprit.tn>
2017-10-25 17:14:55 +0200

    fix mssql + change config

--- olapy/etl/_services.py
+++ olapy/etl/_services.py
@@ -1,40 +1,33 @@
 from collections import defaultdict
 from sqlalchemy import create_engine
+import yaml
 
-# *****mysql default config****
-# DRIVER = 'mysql'
-# HOST = 'localhost'
-# PORT = '3306'
-# DB_NAME = 'sales_mysql'
-# USER = 'root'
-# PASSWORD = 'toor'
-
-# *****postgres default config****
-DRIVER = 'postgres'
-HOST = 'localhost'
-PORT = '5432'
-DB_NAME = 'tutorial'
-USER = 'tutorial'
-PASSWORD = 'tutorial'
+try:
+    config = yaml.load(open('db_config', 'r'))
+except FileExistsError:
+    raise FileExistsError()
 
 DB_CONFIG_DEFAULTS = {
-    'driver': DRIVER,
-    'host': HOST,
-    'port': PORT,
-    'name': DB_NAME,
-    'user': USER,
-    'pass': PASSWORD,
+    # for sql server 'driver':mssql+pyodbc or mssql+pymssql
+    'driver': config['driver'],
+    'host': config['host'],
+    'port': config['port'],
+    'name': config['db_name'],
+    'user': config['user'],
+    'pass': config['password'],
 }
 
 DSN_TEMPLATE = '{driver}://{user}:{pass}@{host}:{port}/{name}'
 
 
-def create_db_engine():
+def create_db_engine(driver='SQL Server Native Client', version='11.0'):
     config = defaultdict(**DB_CONFIG_DEFAULTS)
     dsn = DSN_TEMPLATE.format(**config)
 
     if DB_CONFIG_DEFAULTS['driver'].upper() == 'POSTGRES':
         dsn += '?client_encoding=utf8'
+    elif 'MSSQL' in DB_CONFIG_DEFAULTS['driver'].upper():
+        dsn += '?driver={0}'.format(driver + ' ' + version)
     return create_engine(dsn)
 
 
