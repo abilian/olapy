a1e92124996e904cd75b2234268404debc9a7673
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2017-10-26 16:43:52 +0200

    fix config

--- olapy/core/mdx/tools/olapy_config_file_parser.py
+++ olapy/core/mdx/tools/olapy_config_file_parser.py
@@ -41,7 +41,6 @@ class DbConfigParser:
                 config = yaml.load(config_file)
                 return {
                     'sgbd': config['sgbd'],
-                    'driver': config['driver'] if config['sgbd'].upper() != 'MSSQL' else 'SQL Server Native Client 11.0',
                     'user_name': config['user'],
                     'password': config['password'] if 'LOCALHOST' not in config['user'].upper() else '',
                     'host': config['host'] if 'LOCALHOST' not in config['user'].upper() else '',
