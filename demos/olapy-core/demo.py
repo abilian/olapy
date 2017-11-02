a7c788e93307b76d6d58134ffe0f1eb1f0cd426a
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2017-11-02 16:57:07 +0100

    add olapy demo

---
+++ demos/olapy-core/demo.py
@@ -0,0 +1,14 @@
+from olapy.core.mdx.executor.execute import MdxEngine
+
+mdx_query = """SELECT
+            Hierarchize({[Measures].[Amount]}) ON COLUMNS
+            FROM
+            """
+def main():
+    executor = MdxEngine('sales')
+    executor.mdx_query = mdx_query
+    execution_result = executor.execute_mdx()
+    print(execution_result['result'])
+
+if __name__ == '__main__':
+    main()
