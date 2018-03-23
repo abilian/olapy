89db3d20dfe06abf42dac4a8aa05269d8dd9c483
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-03-23 16:31:31 +0100

    simplify conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,19 +12,13 @@ from olapy.core.mdx.executor.execute import MdxEngine
 @pytest.fixture(scope='module')
 def executor():
     MdxEngine.source_type = ('csv', 'db')
-    db_test = 'main'  # todo change sqlite db name
-    if 'SQLALCHEMY_DATABASE_URI' in os.environ:
-        if os.environ['SQLALCHEMY_DATABASE_URI'] != 'sqlite://':
-            db_test = 'sales_test'  # todo change sqlite db name
-    else:
-        os.environ['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
-
+    sqlalchemy_uri = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite://')
+    db_test = sqlalchemy_uri.split('/')[-1] if sqlalchemy_uri.split('/')[-1] else 'main'
     MdxEngine.source_type = ('csv', 'db')
-
-    engine = sqlalchemy.create_engine(os.environ['SQLALCHEMY_DATABASE_URI'])
+    engine = sqlalchemy.create_engine(sqlalchemy_uri)
     create_insert(engine)
     mdx_engine = MdxEngine(sql_engine=engine, source_type='db')
     mdx_engine.load_cube(cube_name=db_test, fact_table_name='facts')
     yield mdx_engine
     drop_tables(engine)
-    del os.environ['SQLALCHEMY_DATABASE_URI']
+    os.environ.pop('SQLALCHEMY_DATABASE_URI', None)
