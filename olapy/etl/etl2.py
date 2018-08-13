70e744ceea2df9c9ec7bb4f2559f9b4075643421
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-08-13 16:43:50 +0100

    use config file

--- olapy/etl/etl2.py
+++ olapy/etl/etl2.py
@@ -1,8 +1,12 @@
 import os
+from os.path import isdir, expanduser
+from pathlib import Path
 
+import click
 import pandas as pd
 
 import bonobo
+import yaml
 from bonobo.config import use
 
 
@@ -25,11 +29,13 @@ def transform(*args, **kwargs):
     yield olapy_data_set
 
 
-@use('olapy_data_path')
+@use('output_cube_path')
 def load_to_olapy(*args, **kwargs):
     olapy_data_set = args[0]
+    if not isdir(kwargs.get('output_cube_path')):
+        os.mkdir(kwargs.get('output_cube_path'))
     for df_name, df in olapy_data_set.items():
-        save_to = os.path.join(kwargs.get('olapy_data_path'), df_name + '.csv')
+        save_to = os.path.join(kwargs.get('output_cube_path'), df_name + '.csv')
         df.to_csv(path_or_buf=save_to, sep=';', encoding='utf8')
 
 
@@ -49,7 +55,7 @@ def get_graph(**options):
     return graph
 
 
-def get_services(**options):
+def get_services(input_file_path, cube_config, output_cube_path, **options):
     """
     This function builds the services dictionary, which is a simple dict of names-to-implementation used by bonobo
     for runtime injection.
@@ -60,36 +66,38 @@ def get_services(**options):
     :return: dict
     """
     return {
-        'olapy_data_path': options.get('olapy_data_path'),
-        'cube_config': options.get('cube_config'),
-        'input_file_path': options.get('input_file_path'),
+        'output_cube_path': output_cube_path,
+        'cube_config': cube_config,
+        'input_file_path': input_file_path,
     }
 
 
-# The __main__ block actually execute the graph.
-if __name__ == '__main__':
+@click.command()
+@click.option('--input_file_path', '-in_file', default="/home/moddoy/Downloads/Activity Export.Jan.Fev.2018.xlsx",
+              help='Number of greetings.')
+@click.option('--cube_config', '-config', default="etl_conf.yml")
+@click.option('--output_cube_path', '-out_cube', default=None, help='The person to greet.')
+def run_etl(input_file_path, cube_config, output_cube_path):
     parser = bonobo.get_argument_parser()
     with bonobo.parse_args(parser) as options:
-        cube_config = {
-            'Facts': ['Price', 'Product or Service Default Purchase Price', 'Total Price', 'Quantity'],
-            'Accounts': ['Source Account', 'Destination Account'],
-            'Client': ['Client Activity', 'Client', 'Client Role', 'Client Function', 'Client Region',
-                       'Client Province',
-                       'Client Address', 'Client Address City', 'Client Address ZipCode', 'Client Telephone'],
-            'Date': ['Shipping Date', 'Delivery Date', 'Creation Date', 'Modification Date'],
-            'Delivery': ['State', 'Delivery Type', 'Type', 'Owner', 'Delivery Title', 'Delivery Description',
-                         'Delivery Reference'],
-            'Product': ['Product Line', 'Product or Service Reference', 'Product or Service'],
-            'Sales': ['Seller', 'Buyer', 'Reference'],
-            'sender': ['Sender or Provider', 'Recipient or Beneficiary', 'Supplier', 'Invoice Sender'],
-            'Trade': ['Trade Condition Type', 'Trade Condition', 'Trade Condition Reference']
-        }
-        options['cube_config'] = cube_config
 
-        options['input_file_path'] = "/home/moddoy/Downloads/Activity Export.Jan.Fev.2018.xlsx"
+        with open(cube_config) as config_file:
+            options['cube_config'] = yaml.load(config_file)
 
-        options['olapy_data_path'] = "/home/moddoy/olapy-data/cubes/trotinettes"
+        options['input_file_path'] = input_file_path
+
+        if output_cube_path:
+            options['output_cube_path'] = output_cube_path
+        else:
+            options['output_cube_path'] = os.path.join(expanduser('~'),
+                                                       Path(input_file_path).stem)
+
         bonobo.run(
             get_graph(**options),
             services=get_services(**options)
         )
+
+
+# The __main__ block actually execute the graph.
+if __name__ == '__main__':
+    run_etl()
