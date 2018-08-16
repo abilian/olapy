e092c1b51548a9b213ebe7a78f3784e32e0cf948
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-08-16 12:11:31 +0100

    fix: demo etl

--- demos/ETL2/demo.py
+++ demos/ETL2/demo.py
@@ -1,19 +1,25 @@
 import os
 
-# import click
-#
-# from olapy.etl.etl2 import run_etl
+import click
+
+from olapy.etl.etl2 import run_etl
 
 
-def main():
+@click.command()
+@click.pass_context
+def main(ctx):
+    # demo cli etl
     # after python setup.py install
-    os.system("etl --input_file_path=sales.xlsx --config_file=config.yml --output_cube_path='./cube1'")
+    os.system("etl --input_file_path=sales.xlsx --config_file=config.yml --output_cube_path='cube1'")
 
-    # config = {
-    #
-    # }
-    # todo this
-    # run_etl(input_file_path='input_file.xls', cube_config=config, output_cube_path='./cube2')
+    # demo run_etl as function with config as dict
+    config = {
+        'Facts': ['Amount', 'Count'],
+        'Geography': ['Continent', 'Country', 'City'],
+        'Product': ['Company', 'Article', 'Licence'],
+        'Date': ['Year', 'Quarter', 'Month', 'Day']
+    }
+    ctx.invoke(run_etl, input_file_path='sales.xlsx', cube_config=config, output_cube_path='cube2')
 
 
 if __name__ == '__main__':
