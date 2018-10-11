121649dc30c50b2980ac524785424576bdf577d6
mouadh.kaabachi <mouadh.kaabachi@gmail.com>
2018-10-11 16:00:24 +0100

    use gunicorn

--- pyodide/demo/README.rst
+++ pyodide/demo/README.rst
@@ -1,8 +1,15 @@
 Demo
 ----
 
-Run the server with::
+Run the simple server with::
 
     python server.py
+
+or with gunicorn ::
+
+    pip install gunicorn
+
+    gunicorn -w 4 server:app
+
 
 and go to http://localhost:8000/olapy.html
--- pyodide/demo/server.py
+++ pyodide/demo/server.py
@@ -8,6 +8,7 @@ import socketserver
 
 PORT = 8000
 
+
 class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
@@ -17,11 +18,24 @@ class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         Handler.extensions_map['.wasm'] = 'application/wasm'
         Handler.end_headers(self)
 
+
 def server(port):
     httpd = socketserver.TCPServer(('', port), HTTPRequestHandler)
     return httpd
 
+
+def app(environ, start_response):
+    current_folder_path, current_folder_name = os.path.split(os.getcwd())
+    if not current_folder_name == '_build':
+        os.chdir('_build')
+    port = PORT
+    httpd = server(port)
+    print("\nserving from _build/ at localhost:" + str(port))
+    httpd.serve_forever()
+
+
 if __name__ == "__main__":
+
     port = PORT
     httpd = server(port)
     try:
