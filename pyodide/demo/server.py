368248370b503b5a3eb7a173def6bc599e21a64d
mouadh.kaabachi <mouadh.kaabachi@gmail.com>
2018-10-12 10:38:25 +0100

    fix serve csv files

--- pyodide/demo/server.py
+++ pyodide/demo/server.py
@@ -1,5 +1,4 @@
 from wsgiref.simple_server import make_server
-import os
 
 
 def content_type(path):
@@ -9,34 +8,32 @@ def content_type(path):
         return "text/javascript"
     elif path.endswith('.wasm'):
         return 'application/wasm'
+    elif path.endswith('.csv'):
+        return 'text/csv'
     else:
         return "text/html"
 
 
 def app(environ, start_response):
     path_info = environ["PATH_INFO"]
-    resource = path_info.split("/")[1]
+
+    if path_info == '/':
+        path_info = "/olapy.html"
+
+    resource = "_build" + path_info
 
     headers = []
-    headers.append(("Content-Type", content_type(resource)))
+    headers.append(("Content-Type", content_type(path_info.split("/")[-1])))
     headers.append(('Access-Control-Allow-Origin', '*'))
 
-    if not resource:
-        resource = "olapy.html"
-
-    resp_file = os.path.join("_build", resource)
-
-    try:
-        with open(resp_file, "rb") as f:
-            resp_file = f.read()
-    except Exception:
-        start_response("404 Not Found", headers)
-        return ["404 Not Found"]
+    with open(resource, "rb") as f:
+        resp_file = f.read()
 
     start_response("200 OK", headers)
     return [resp_file]
 
 
 def runserver(environ, start_response):
+    # http://127.0.0.1:8080/cubes/sales/Facts.csv
     server = make_server("0.0.0.0", 8080, app)
     server.serve_forever()
