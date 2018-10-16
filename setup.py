28b58699811a04fdf14f90923f284e578d5ecfab
mouadh.kaabachi <mouadh.kaabachi@gmail.com>
2018-10-16 17:04:04 +0100

    sqlachmey as extra req

--- setup.py
+++ setup.py
@@ -29,6 +29,9 @@ setup(
         "Programming Language :: Python :: 3.6",
         # "Topic :: Business intelligence",
     ],
+    extras_require={
+        'db': ["SQLAlchemy"],
+    },
     entry_points={'console_scripts': ['olapy = olapy.__main__:cli',
                                       'etl = olapy.etl.etl:run_etl', ]},
 )
