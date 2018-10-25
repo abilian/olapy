0684c04b23286717ddccbdf9f9aa46b46002326a
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-10-25 16:33:51 +0100

    forgotten wsgi server update

--- pyodide/build_olapy_by_yourself/server.py
+++ pyodide/build_olapy_by_yourself/server.py
@@ -1,33 +1,27 @@
-# Python http.server that sets Access-Control-Allow-Origin header.
-# https://gist.github.com/razor-x/9542707
+import mimetypes
+from wsgiref.simple_server import make_server
 
-import os
-import sys
-import http.server
-import socketserver
 
-PORT = 8000
+def app(environ, start_response):
+    path_info = environ["PATH_INFO"]
+    mimetypes.add_type('application/wasm', '.wasm')
+    mimetypes.add_type('text/html', '.data')
+    if path_info == '/':
+        path_info = "/olapy.html"
 
-class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def end_headers(self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        # http.server.SimpleHTTPRequestHandler
-        Handler = http.server.SimpleHTTPRequestHandler
-        Handler.extensions_map['.wasm'] = 'application/wasm'
-        Handler.end_headers(self)
+    resource = "_build" + path_info
+    headers = []
+    headers.append(("Content-Type", mimetypes.guess_type(path_info.split("/")[-1])[0]))
+    headers.append(('Access-Control-Allow-Origin', '*'))
 
-def server(port):
-    httpd = socketserver.TCPServer(('', port), HTTPRequestHandler)
-    return httpd
+    with open(resource, "rb") as f:
+        resp_file = f.read()
 
-if __name__ == "__main__":
-    port = PORT
-    httpd = server(port)
-    try:
-        os.chdir('build')
-        print("\nserving from build/ at localhost:" + str(port))
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        print("\n...shutting down http server")
-        httpd.shutdown()
-sys.exit()
+    start_response("200 OK", headers)
+    return [resp_file]
+
+
+def runserver(environ, start_response):
+    # http://127.0.0.1:8080/cubes/sales/Facts.csv
+    server = make_server("0.0.0.0", 8080, app)
+    server.serve_forever()
--- pyodide/demo/server.py
+++ pyodide/demo/server.py
@@ -22,6 +22,5 @@ def app(environ, start_response):
 
 
 def runserver(environ, start_response):
-    # http://127.0.0.1:8080/cubes/sales/Facts.csv
     server = make_server("0.0.0.0", 8080, app)
     server.serve_forever()
