577cd6f7a70df97a956c29f07b8fa9b025e7a70f
mouadhkaabachi <mouadh.kaabachi@esprit.tn>
2018-12-21 12:55:04 +0100

    fix write logs in file

--- olapy/core/mdx/executor/__init__.py
+++ olapy/core/mdx/executor/__init__.py
@@ -1,19 +1,22 @@
-import logging
-
 try:
     # if pyspark install, use it instead of pandas
 
     from .spark.cube_loader import SparkCubeLoader as CubeLoader
     from .spark.execute import SparkMdxEngine as MdxEngine
 
-    logging.warning(' **************************************************** ')
-    logging.warning(' ***************** OlaPy with Spark ***************** ')
-    logging.warning(' **************************************************** ')
+    print("""
+    ****************************************************
+    ***************** OlaPy with Spark *****************
+    ****************************************************
+    """)
+
 except ImportError:
 
     from .cube_loader import CubeLoader  # type: ignore
     from .execute import MdxEngine  # type: ignore
 
-    logging.warning(' ***************************************************** ')
-    logging.warning(' ***************** OlaPy with Pandas ***************** ')
-    logging.warning(' ***************************************************** ')
+    print("""
+        ****************************************************
+        ***************** OlaPy with Pandas ****************
+        ****************************************************
+        """)
