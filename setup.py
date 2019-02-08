c8fbfea1cd58746bdc1d1642222f714043797dbe
Stefane Fermigier <sf@fermigier.com>
2019-02-08 17:40:38 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [str(req) for req in _install_requires]
 
 setup(
     name='olapy',
-    version="0.7.0",
+    version="0.7.5",
     packages=find_packages(),
     author="Abilian SAS",
     author_email="contact@abilian.com",
