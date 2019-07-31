75c07feaf1178d6f7937328d1a0e65de97c3ff5d
Stefane Fermigier <sf@fermigier.com>
2019-07-31 09:38:48 +0200

    refactor: group similar tests in same module.

--- tests/test_xmla_responses.py
+++ tests/test_xmla_responses.py
@@ -7,12 +7,135 @@ import xmlwitch
 from olapy.core.services.xmla_execute_request_handler import \
     XmlaExecuteReqHandler
 
-from .queries import query15
+from .queries import query11, query12, query14, query15
 
 sqlalchemy = pytest.importorskip("sqlalchemy")
 
 
-def test_xs0_axis_query15(executor):
+#
+# Test slicer
+#
+def test_slicer_axis_query11(executor):
+    """
+    One measure.
+    """
+    xml = xmlwitch.Builder()
+    with xml.Axis(name="SlicerAxis"):
+        with xml.Tuples:
+            with xml.Tuple:
+                with xml.Member(Hierarchy="[geography].[geography]"):
+                    xml.UName("[geography].[geography].[continent].[America]")
+                    xml.Caption("America")
+                    xml.LName("[geography].[geography].[continent]")
+                    xml.LNum("0")
+                    xml.DisplayInfo("2")
+                with xml.Member(Hierarchy="[product].[product]"):
+                    xml.UName("[product].[product].[company].[Crazy Development]")
+                    xml.Caption("Crazy Development")
+                    xml.LName("[product].[product].[company]")
+                    xml.LNum("0")
+                    xml.DisplayInfo("2")
+                with xml.Member(Hierarchy="[time].[time]"):
+                    xml.UName("[time].[time].[year].[2010]")
+                    xml.Caption("2010")
+                    xml.LName("[time].[time].[year]")
+                    xml.LNum("0")
+                    xml.DisplayInfo("2")
+                with xml.Member(Hierarchy="[Measures]"):
+                    xml.UName("[Measures].[amount]")
+                    xml.Caption("amount")
+                    xml.LName("[Measures]")
+                    xml.LNum("0")
+                    xml.DisplayInfo("0")
+
+    xmla_tools = XmlaExecuteReqHandler(executor, query11, False)
+    assert str(xml) == xmla_tools.generate_slicer_axis()
+
+
+def test_slicer_axis_query12(executor):
+    """
+    Many measures.
+    """
+    xml = xmlwitch.Builder()
+    with xml.Axis(name="SlicerAxis"):
+        with xml.Tuples:
+            with xml.Tuple:
+                with xml.Member(Hierarchy="[geography].[geography]"):
+                    xml.UName("[geography].[geography].[continent].[America]")
+                    xml.Caption("America")
+                    xml.LName("[geography].[geography].[continent]")
+                    xml.LNum("0")
+                    xml.DisplayInfo("2")
+                with xml.Member(Hierarchy="[product].[product]"):
+                    xml.UName("[product].[product].[company].[Crazy Development]")
+                    xml.Caption("Crazy Development")
+                    xml.LName("[product].[product].[company]")
+                    xml.LNum("0")
+                    xml.DisplayInfo("2")
+                with xml.Member(Hierarchy="[time].[time]"):
+                    xml.UName("[time].[time].[year].[2010]")
+                    xml.Caption("2010")
+                    xml.LName("[time].[time].[year]")
+                    xml.LNum("0")
+                    xml.DisplayInfo("2")
+
+    xmla_tools = XmlaExecuteReqHandler(executor, query12, False)
+    assert str(xml) == xmla_tools.generate_slicer_axis()
+
+
+def test_slicer_axis_query14(executor):
+    """
+    Dimension with all measures.
+    """
+    xml = xmlwitch.Builder()
+    with xml.Axis(name="SlicerAxis"):
+        with xml.Tuples:
+            with xml.Tuple:
+                with xml.Member(Hierarchy="[product].[product]"):
+                    xml.UName("[product].[product].[company].[Crazy Development]")
+                    xml.Caption("Crazy Development")
+                    xml.LName("[product].[product].[company]")
+                    xml.LNum("0")
+                    xml.DisplayInfo("2")
+                with xml.Member(Hierarchy="[time].[time]"):
+                    xml.UName("[time].[time].[year].[2010]")
+                    xml.Caption("2010")
+                    xml.LName("[time].[time].[year]")
+                    xml.LNum("0")
+                    xml.DisplayInfo("2")
+
+    xmla_tools = XmlaExecuteReqHandler(executor, query14, False)
+    assert str(xml) == xmla_tools.generate_slicer_axis()
+
+
+#
+# Test cell data
+#
+def test_query15_cell_data(executor):
+    """
+    select ()()()...
+    """
+    xml = xmlwitch.Builder()
+    with xml.Cell(CellOrdinal=str(0)):
+        xml.Value(str(8), **{"xsi:type": "xsi:long"})
+    with xml.Cell(CellOrdinal=str(1)):
+        xml.Value(str(144), **{"xsi:type": "xsi:long"})
+    with xml.Cell(CellOrdinal=str(2)):
+        xml.Value(str(3), **{"xsi:type": "xsi:long"})
+    with xml.Cell(CellOrdinal=str(3)):
+        xml.Value(str(4), **{"xsi:type": "xsi:long"})
+    with xml.Cell(CellOrdinal=str(4)):
+        xml.Value(str(96), **{"xsi:type": "xsi:long"})
+
+    xmla_tools = XmlaExecuteReqHandler(executor, query15, False)
+
+    assert str(xml) == xmla_tools.generate_cell_data()
+
+
+#
+# Test xs0 responses
+#
+def test_query15_xs0(executor):
     """
     select ()()()...
     """
